2003a33dcb130669218c3ce9c6c490760237e903
Chris Heuberger <chrisheuberg@gmail.com>
2020-04-12 15:18:50 -0400

    round out questions

--- dist/script.js
+++ dist/script.js
@@ -1,16 +1,24 @@
 let numbers = [1, 2, 3, 4, 5, 6];
 let data = {
   "questions": [{
+  "question": "How many members of Congress serve in the House of Representatives? 538, 100, 250, or 435?",
+  "answer": "435"
+  },
+  {
+  "question": "What is the definition of&nbsp;<em>peregrination</em>?",
+  "answer": "A journey, especially a long or meandering one."
+  },
+  {
   "question": "Which animal has the longest neck?",
   "answer": "Giraffe"
   },
   {
-  "question": "What was the name of Vincent Van Gogh’s brother?",
-  "answer": "Theo"
-  },
-  {
   "question": "How many sides does a triangle have?",
   "answer": "Three"
+  },
+  {
+  "question": "What was the name of Vincent Van Gogh’s brother?",
+  "answer": "Theo"
   },
   {
   "question": "What was the name of the ship Darwin took to the Galapagos and what year was his first voyage on it? (guess can be within three years)",
@@ -29,12 +37,12 @@ let data = {
   "answer": "Martha Washington, Abigail Adams, Martha Jefferson, and Dolley Madison."
   },
   {
-  "question": "Forget the number you rolled. Move one space for each of the five (sometimes six) vowels in the alphabet you can name.",
-  "answer": "A, E, I, O, U, and sometimes Y"
-  },
-  {
   "question": "Daily Triple! Who painted this painting? Bonus for an extra space: what is the painting’s title? Bonus for six extra spaces: when was it painted?<img class='question-img' src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/219932/Las-Meninas-Diego-Velazquez.jpg'>",
   "answer": "<em>Las Meninas</em> by Velázquez, 1656"
+  },
+  {
+  "question": "Forget the number you rolled. Move one space for each of the five (sometimes six) vowels in the alphabet you can name.",
+  "answer": "A, E, I, O, U, and sometimes Y"
   },
   {
   "question": "What is the name of Peppa Pig's little brother?",
@@ -45,24 +53,20 @@ let data = {
   "answer": "A parliament"
   },
   {
+  "question": "Forget the number you rolled. Move one space for each of the five Great Lakes you can name.",
+  "answer": "Superior, Huron, Michigan, Ontario and Erie"
+  },
+  {
   "question": "Which ocean is off the Oregon coast?",
   "answer": "The Pacific"
-  },
-  {
-  "question": "What is the name of the supermassive black hole at the center of the Milky Way galaxy?",
-  "answer": "Sagittarius A*"
-  },
-  {
-  "question": "It's a gimme! If anyone on your team has eaten a vegetable today, you may move the amount you rolled.",
-  "answer": "No answer necessary"
   },
   {
   "question": "If you freeze water, what do you get?",
   "answer": "Ice."
   },
   {
-  "question": "Forget the number you rolled. Move one space for each of the five Great Lakes you can name.",
-  "answer": "Superior, Huron, Michigan, Ontario and Erie"
+  "question": "What is the name of the supermassive black hole at the center of the Milky Way galaxy?",
+  "answer": "Sagittarius A*"
   },
   {
   "question": "How long is a zeptosecond? A thousandth of a second, a billionth of a second, a trillionth of a second, or a trillionth of a billionth of a second?",
@@ -73,6 +77,14 @@ let data = {
   "answer": "Butterflies"
   },
   {
+  "question": "It's a gimme! If anyone on your team has eaten a vegetable today, you may move the amount you rolled.",
+  "answer": "No answer necessary"
+  },
+  {
+  "question": "Daily Triple! Who painted this painting? Bonus for an extra space: what is the painting’s title? Bonus for six extra spaces: when was it painted?<img class='question-img' src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/219932/The-Night-Watch-Rembrandt.webp'>",
+  "answer": "<em>The Night Watch</em> by Rembrandt, 1642"
+  },
+  {
   "question": "Complete the stanza: \"The woods are lovely, dark and deep...\"",
   "answer": "But I have promises to keep,<br>And miles to go before I sleep<br>And miles to go before I sleep."
   },
@@ -81,48 +93,40 @@ let data = {
   "answer": "9"
   },
   {
-  "question": "Name any year during which the Lewis and Clark Expedition took place.",
-  "answer": "The Lewis and Clark Expedition (also known as the Corps of Discovery Expedition) spanned from August 31, 1803 to September 25, 1806."
-  },
-  {
   "question": "Which country is home to the kangaroo?",
   "answer": "Australia"
   },
   {
-  "question": "Daily Triple! Who painted this painting? Bonus for an extra space: what is the painting’s title? Bonus for six extra spaces: when was it painted?<img class='question-img' src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/219932/The-Night-Watch-Rembrandt.webp'>",
-  "answer": "<em>The Night Watch</em> by Rembrandt, 1642"
-  },
-  {
-  "question": "What two countries are neighbors of the United States?",
-  "answer": "Canada and Mexico"
+  "question": "Name any year during which the Lewis and Clark Expedition took place.",
+  "answer": "The Lewis and Clark Expedition (also known as the Corps of Discovery Expedition) spanned from August 31, 1803 to September 25, 1806."
   },
   {
   "question": "Who said, \"E pur si muove\" (\"And yet it moves\")?",
   "answer": "Galileo Galilei in 1633 after being forced to recant his claims that the Earth moves around the Sun, rather than the converse."
   },
   {
+  "question": "What two countries are neighbors of the United States?",
+  "answer": "Canada and Mexico"
+  },
+  {
   "question": "What is 5 + 5 - 5 - 5?",
   "answer": "0"
   },
   {
-  "question": "What are the colors of the rainbow?",
-  "answer": "Red, orange, yellow, green, blue, indigo, violet (will accept purple)"
-  },
-  {
-  "question": "How many members of Congress serve in the House of Representatives? 538, 100, 250, or 435?",
-  "answer": "435"
-  },
-  {
-  "question": "How many legs does a spider have?",
-  "answer": "Eight."
+  "question": "What is the definition of&nbsp;<em>metadiegetic</em>?",
+  "answer": "Self-assertive or proud to an irritating degree."
   },
   {
   "question": "What was the first pizzeria in the United States?",
   "answer": "Lombardi's Pizza on Spring Street at Mott Street in New York City's Nolita neighborhood."
   },
   {
-  "question": "Name three animals that lay eggs.",
-  "answer": "Birds like chickens, penguins, parrots, and woodpeckers; reptiles like snakes, lizards, turtles, and alligators; amphibians like frogs and salamanders; fish like salmon, marlin, and goldfish; and even platypuses!"
+  "question": "What are the colors of the rainbow?",
+  "answer": "Red, orange, yellow, green, blue, indigo, violet (will accept purple)"
+  },
+  {
+  "question": "How many legs does a spider have?",
+  "answer": "Eight."
   },
   {
   "question": "Who was the first person to run a mile in under four minutes? Bonus for an extra space: what year did they do it?",
@@ -133,6 +137,10 @@ let data = {
   "answer": "Voting Rights for All Races"
   },
   {
+  "question": "Name three animals that lay eggs.",
+  "answer": "Birds like chickens, penguins, parrots, and woodpeckers; reptiles like snakes, lizards, turtles, and alligators; amphibians like frogs and salamanders; fish like salmon, marlin, and goldfish; and even platypuses!"
+  },
+  {
   "question": "Name three fruits.",
   "answer": "Banana, berries, orange, apple, grapes, tomatoes, avocadoes, etc."
   },
@@ -141,12 +149,12 @@ let data = {
   "answer": "299,792,458 meters per second"
   },
   {
-  "question": "In the movie <em>Frozen</em>, what season does Olaf sing about?",
-  "answer": "Summer"
-  },
-  {
   "question": "Name all the planets in our solar system in order.",
   "answer": "Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, and Neptune"
+  },
+  {
+  "question": "In the movie <em>Frozen</em>, what season does Olaf sing about?",
+  "answer": "Summer"
   },
   {
   "question": "What kind of vegetable is this?<img class='question-img' src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/219932/eggplant.jpg'>",
@@ -157,12 +165,12 @@ let data = {
   "answer": "Tetrahedron, cube, octahedron, dodecahedron, and icosahedron"
   },
   {
-  "question": "What kid of house do birds build?",
-  "answer": "A nest"
-  },
-  {
   "question": "Daily Triple! What is the Reinheitsgebot? Bonus for an extra space: what does it decree? Bonus for six extra spaces: when was it issued?",
   "answer": "It is the German Beer purity Law issued by Duke Wilhelm IV Bavaria in 1516 and decreed that beer shall only contain barley, hops and water (they didn't know about yeast yet)."
+  },
+  {
+  "question": "What kid of house do birds build?",
+  "answer": "A nest"
   },
   {
   "question": "How many sides does a rectangle have?",
@@ -171,6 +179,18 @@ let data = {
   {
   "question": "Which item was <em>not</em> invented at The Bell Labs Building on Bethune Street in the West Village? The transistor, amplifiers, vaccuum tubes, solar panels, lasers, or voice scramblers?",
   "answer": "Lasers"
+  },
+  {
+  "question": "What is the definition of&nbsp;<em>bumptious</em>?",
+  "answer": "Self-assertive or proud to an irritating degree."
+  },
+  {
+  "question": "How many wheels does a car have?",
+  "answer": "Four (will accept five including steering wheel)"
+  },
+  {
+  "question": "What does a bear do in winter?",
+  "answer": "Hibernate."
   }]
 }
 
