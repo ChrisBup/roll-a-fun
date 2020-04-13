b3adb29719f36d8f8222792968fe5d525e04c7dc
Chris Heuberger <chrisheuberg@gmail.com>
2020-04-12 21:12:01 -0400

    p tags

--- dist/script.js
+++ dist/script.js
@@ -1,195 +1,195 @@
 let numbers = [1, 2, 3, 4, 5, 6];
 let data = {
   "questions": [{
-  "question": "How many members of Congress serve in the House of Representatives? 538, 100, 250, or 435?",
+  "question": "<p>How many members of Congress serve in the House of Representatives? 538, 100, 250, or 435?</p>",
   "answer": "435"
   },
   {
-  "question": "What is the definition of&nbsp;<em>peregrination</em>?",
+  "question": "<p>What is the definition of&nbsp;<em>peregrination</em>?</p>",
   "answer": "A journey, especially a long or meandering one"
   },
   {
-  "question": "Which animal has the longest neck?",
+  "question": "<p>Which animal has the longest neck?</p>",
   "answer": "Giraffe"
   },
   {
-  "question": "How many sides does a triangle have?",
+  "question": "<p>How many sides does a triangle have?</p>",
   "answer": "Three"
   },
   {
-  "question": "What was the name of Vincent Van Gogh’s brother?",
+  "question": "<p>What was the name of Vincent Van Gogh’s brother?</p>",
   "answer": "Theo"
   },
   {
-  "question": "What was the name of the ship Darwin took to the Galapagos and what year was his first voyage on it? (guess can be within three years)",
+  "question": "<p>What was the name of the ship Darwin took to the Galapagos and what year was his first voyage on it? (guess can be within three years)</p>",
   "answer": "H.M.S. Beagle, 1835"
   },
   {
-  "question": "It's a gimme! If someone on your team is wearing something purple right now, you may move the amount you rolled. Bonus: if it's purple polka dots, add an extra two spaces.",
+  "question": "<p>It's a gimme! If someone on your team is wearing something purple right now, you may move the amount you rolled. Bonus: if it's purple polka dots, add an extra two spaces.</p>",
   "answer": "No answer necessary"
   },
   {
-  "question": "What is the name of the island at the toe of the boot of Italy?",
+  "question": "<p>What is the name of the island at the toe of the boot of Italy?</p>",
   "answer": "Sicilia (will accept Sicily)"
   },
   {
-  "question": "What were the names of the wives of the first four presidents of the United States?",
+  "question": "<p>What were the names of the wives of the first four presidents of the United States?</p>",
   "answer": "Martha Washington, Abigail Adams, Martha Jefferson, and Dolley Madison."
   },
   {
-  "question": "Daily Triple! Who painted this painting? Bonus for an extra space: what is the painting’s title? Bonus for six extra spaces: when was it painted?<img class='question-img' src='./img/Las-Meninas-Diego-Velazquez.jpg'>",
+  "question": "<p>Daily Triple! Who painted this painting? Bonus for an extra space: what is the painting’s title? Bonus for six extra spaces: when was it painted?</p><img class='question-img' src='./img/Las-Meninas-Diego-Velazquez.jpg'>",
   "answer": "<em>Las Meninas</em> by Velázquez, 1656"
   },
   {
-  "question": "Forget the number you rolled. Move one space for each of the five (sometimes six) vowels in the alphabet you can name.",
+  "question": "<p>Forget the number you rolled. Move one space for each of the five (sometimes six) vowels in the alphabet you can name.</p>",
   "answer": "A, E, I, O, U, and sometimes Y"
   },
   {
-  "question": "What is the name of Peppa Pig's little brother?",
+  "question": "<p>What is the name of Peppa Pig's little brother?</p>",
   "answer": "George"
   },
   {
-  "question": "What is a group of owls called? A hootenanny, a parliament, or a sleuth?",
+  "question": "<p>What is a group of owls called? A hootenanny, a parliament, or a sleuth?</p>",
   "answer": "A parliament"
   },
   {
-  "question": "Forget the number you rolled. Move one space for each of the five Great Lakes you can name.",
+  "question": "<p>Forget the number you rolled. Move one space for each of the five Great Lakes you can name.</p>",
   "answer": "Superior, Huron, Michigan, Ontario and Erie"
   },
   {
-  "question": "Which ocean is off the Oregon coast?",
+  "question": "<p>Which ocean is off the Oregon coast?</p>",
   "answer": "The Pacific"
   },
   {
-  "question": "If you freeze water, what do you get?",
+  "question": "<p>If you freeze water, what do you get?</p>",
   "answer": "Ice"
   },
   {
-  "question": "What is the name of the supermassive black hole at the center of the Milky Way galaxy?",
+  "question": "<p>What is the name of the supermassive black hole at the center of the Milky Way galaxy?</p>",
   "answer": "Sagittarius A*"
   },
   {
-  "question": "How long is a zeptosecond? A thousandth of a second, a billionth of a second, a trillionth of a second, or a trillionth of a billionth of a second?",
+  "question": "<p>How long is a zeptosecond? A thousandth of a second, a billionth of a second, a trillionth of a second, or a trillionth of a billionth of a second?</p>",
   "answer": "A zeptosecond is a trillionth of a billionth of a second. It's the time that a Higgs Boson exists in experiments at the LHC. A millisecond is a thousandth of a second. A nanosecond is a billionth of a second. And a picosecond is a trillionth of a second."
   },
   {
-  "question": "What do caterpillars turn into?",
+  "question": "<p>What do caterpillars turn into?</p>",
   "answer": "Butterflies"
   },
   {
-  "question": "It's a gimme! If anyone on your team has eaten a vegetable today, you may move the amount you rolled.",
+  "question": "<p>It's a gimme! If anyone on your team has eaten a vegetable today, you may move the amount you rolled.</p>",
   "answer": "No answer necessary"
   },
   {
-  "question": "Daily Triple! Who painted this painting? Bonus for an extra space: what is the painting’s title? Bonus for six extra spaces: when was it painted?<img class='question-img' src='./img/The-Night-Watch-Rembrandt.webp'>",
+  "question": "<p>Daily Triple! Who painted this painting? Bonus for an extra space: what is the painting’s title? Bonus for six extra spaces: when was it painted?</p><img class='question-img' src='./img/The-Night-Watch-Rembrandt.webp'>",
   "answer": "<em>The Night Watch</em> by Rembrandt, 1642"
   },
   {
-  "question": "Complete the stanza: \"The woods are lovely, dark and deep...\"",
+  "question": "<p>Complete the stanza: \"The woods are lovely, dark and deep...\"</p>",
   "answer": "But I have promises to keep,<br>And miles to go before I sleep,<br>And miles to go before I sleep."
   },
   {
-  "question": "What is 3 + 3 + 3?",
+  "question": "<p>What is 3 + 3 + 3?</p>",
   "answer": "9"
   },
   {
-  "question": "Which country is home to the kangaroo?",
+  "question": "<p>Which country is home to the kangaroo?</p>",
   "answer": "Australia"
   },
   {
-  "question": "Name any year during which the Lewis and Clark Expedition took place.",
+  "question": "<p>Name any year during which the Lewis and Clark Expedition took place.</p>",
   "answer": "The Lewis and Clark Expedition (also known as the Corps of Discovery Expedition) spanned from August 31, 1803 to September 25, 1806."
   },
   {
-  "question": "Who said, \"E pur si muove\" (\"And yet it moves\")?",
+  "question": "<p>Who said, \"E pur si muove\" (\"And yet it moves\")?</p>",
   "answer": "Galileo Galilei in 1633 after being forced to recant his claims that the Earth moves around the Sun, rather than the converse."
   },
   {
-  "question": "What two countries are neighbors of the United States?",
+  "question": "<p>What two countries are neighbors of the United States?</p>",
   "answer": "Canada and Mexico"
   },
   {
-  "question": "What is 5 + 5 - 5 - 5?",
+  "question": "<p>What is 5 + 5 - 5 - 5?</p>",
   "answer": "0"
   },
   {
-  "question": "What is the definition of&nbsp;<em>metadiegetic</em>?",
+  "question": "<p>What is the definition of&nbsp;<em>metadiegetic</em>?</p>",
   "answer": "Pertaining to a secondary narrative embedded within the primary narrative (a story within a story)"
   },
   {
-  "question": "What was the first pizzeria in the United States?",
+  "question": "<p>What was the first pizzeria in the United States?</p>",
   "answer": "Lombardi's Pizza on Spring Street at Mott Street in New York City's Nolita neighborhood."
   },
   {
-  "question": "What are the colors of the rainbow?",
+  "question": "<p>What are the colors of the rainbow?</p>",
   "answer": "Red, orange, yellow, green, blue, indigo, violet (will accept purple)"
   },
   {
-  "question": "How many legs does a spider have?",
+  "question": "<p>How many legs does a spider have?</p>",
   "answer": "Eight"
   },
   {
-  "question": "Who was the first person to run a mile in under four minutes? Bonus for an extra space: what year did they do it?",
+  "question": "<p>Who was the first person to run a mile in under four minutes? Bonus for an extra space: what year did they do it?</p>",
   "answer": "Roger Bannister in 1954"
   },
   {
-  "question": "What is the 15th Amendment? Slavery outlawed, rights of citizenship, voting rights for all races, federal income taxes, or the election of senators by popular vote?",
+  "question": "<p>What is the 15th Amendment? Slavery outlawed, rights of citizenship, voting rights for all races, federal income taxes, or the election of senators by popular vote?</p>",
   "answer": "Voting rights for all races"
   },
   {
-  "question": "Name three animals that lay eggs.",
+  "question": "<p>Name three animals that lay eggs.</p>",
   "answer": "Birds like chickens, penguins, parrots, and woodpeckers; reptiles like snakes, lizards, turtles, and alligators; amphibians like frogs and salamanders; fish like salmon, marlin, and goldfish; and even platypuses!"
   },
   {
-  "question": "Name three fruits.",
+  "question": "<p>Name three fruits.</p>",
   "answer": "Bananas, berries, oranges, apples, grapes, tomatoes, avocadoes, etc."
   },
   {
-  "question": "What is the speed of light (c)? 276,314 meters per second, 299,792,458 meters per second, or 1,000,000 meters per second?",
+  "question": "<p>What is the speed of light (c)? 276,314 meters per second, 299,792,458 meters per second, or 1,000,000 meters per second?</p>",
   "answer": "299,792,458 meters per second"
   },
   {
-  "question": "Name all the planets in our solar system in order.",
+  "question": "<p>Name all the planets in our solar system in order.</p>",
   "answer": "Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, and Neptune"
   },
   {
-  "question": "In the movie&nbsp;<em>Frozen</em>, what season does Olaf sing about?",
+  "question": "<p>In the movie&nbsp;<em>Frozen</em>, what season does Olaf sing about?</p>",
   "answer": "Summer"
   },
   {
-  "question": "What kind of vegetable is this?<img class='question-img' src='./img/eggplant.jpg'>",
+  "question": "<p>What kind of vegetable is this?</p><img class='question-img' src='./img/eggplant.jpg'>",
   "answer": "An eggplant"
   },
   {
-  "question": "Forget the number you rolled. Move one space for each of the five Platonic ployhedrons you can name.",
+  "question": "<p>Forget the number you rolled. Move one space for each of the five Platonic ployhedrons you can name.</p>",
   "answer": "Tetrahedron, cube, octahedron, dodecahedron, and icosahedron"
   },
   {
-  "question": "Daily Triple! What is the Reinheitsgebot? Bonus for an extra space: what does it decree? Bonus for six extra spaces: when was it issued?",
+  "question": "<p>Daily Triple! What is the Reinheitsgebot? Bonus for an extra space: what does it decree? Bonus for six extra spaces: when was it issued?</p>",
   "answer": "It is the German beer purity Law issued by Duke Wilhelm IV Bavaria in 1516 and decreed that beer shall only contain barley, hops and water (they didn't know about yeast yet)."
   },
   {
-  "question": "What kind of house do birds build?",
+  "question": "<p>What kind of house do birds build?</p>",
   "answer": "A nest"
   },
   {
-  "question": "How many sides does a rectangle have?",
+  "question": "<p>How many sides does a rectangle have?</p>",
   "answer": "Four"
   },
   {
-  "question": "Which item was&nbsp;<em>not</em> invented at The Bell Labs Building on Bethune Street in the West Village? The transistor, amplifiers, vaccuum tubes, solar panels, lasers, or voice scramblers?",
+  "question": "<p>Which item was&nbsp;<em>not</em> invented at The Bell Labs Building on Bethune Street in the West Village? The transistor, amplifiers, vaccuum tubes, solar panels, lasers, or voice scramblers?</p>",
   "answer": "Lasers"
   },
   {
-  "question": "What is the definition of&nbsp;<em>bumptious</em>?",
+  "question": "<p>What is the definition of&nbsp;<em>bumptious</em>?</p>",
   "answer": "Self-assertive or proud to an irritating degree"
   },
   {
-  "question": "How many wheels does a car have?",
+  "question": "<p>How many wheels does a car have?</p>",
   "answer": "Four (will accept five including steering wheel)"
   },
   {
-  "question": "What does a bear do in winter?",
+  "question": "<p>What does a bear do in winter?</p>",
   "answer": "Hibernate"
   }]
 }
